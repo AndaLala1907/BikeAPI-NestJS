9f2e53650a28ea05e7cf6dca902c725cba4f18a6
Anda Lala <andalala07@gmail.com>
2025-06-19 12:43:02 +0200

    removed static PORT to allow Render dynamic port

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   setupSwagger(app);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
